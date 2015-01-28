7a5207dc7f36eb843389b8edbc6389a8dad7a285
Philipp Grogg <grogg@puzzle.ch>
2015-01-28 16:02:25 +0100

    profiler with simple premain method

---
+++ src/ch/puzzle/profiler/Profiler.java
@@ -0,0 +1,9 @@
+package ch.puzzle.profiler;
+
+import java.lang.instrument.Instrumentation;
+
+public class Profiler {
+	public static void premain(String agentArgs, Instrumentation inst) {
+		System.out.println("premain");
+	}
+}
