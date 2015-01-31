7838508d9cfd4db1fb8c4fd3f21f6b2800158732
Philipp Grogg <grogg@puzzle.ch>
2015-01-31 18:27:44 +0100

    refactoring

--- src/ch/puzzle/profiler/MethodCallClassFileTransformer.java
+++ src/ch/puzzle/profiler/MethodCallClassFileTransformer.java
@@ -14,34 +14,38 @@ import javassist.NotFoundException;
 
 public class MethodCallClassFileTransformer implements ClassFileTransformer {
 
+	private ClassPool pool;
+
 	@Override
 	public byte[] transform(ClassLoader loader, String className,
 			Class<?> classBeingRedefined, ProtectionDomain protectionDomain,
 			byte[] classfileBuffer) throws IllegalClassFormatException {
 
 		System.out.println(" Loading class: " + className);
-
-		ClassPool pool = ClassPool.getDefault();
-		pool.insertClassPath(new ByteArrayClassPath(className, classfileBuffer));
-		CtClass ctClass = null;
-		try {
-			ctClass = pool.get(className.replaceAll("/", "."));
-			if (!ctClass.isFrozen()) {
-				for (CtMethod declaredMethod : ctClass.getDeclaredMethods()) {
-					declaredMethod.insertBefore(
-							createEnterInstrumentationString(className, declaredMethod.getName()));
-					declaredMethod.insertAfter(createExitInstrumentationString(className, declaredMethod.getName()));
-				}
-				return ctClass.toBytecode();
+		if (className.startsWith("ch/puzzle/sample/")) {
+			pool = ClassPool.getDefault();
+			pool.insertClassPath(new ByteArrayClassPath(className, classfileBuffer));
+			try {
+				CtClass ctClass = pool.get(className.replaceAll("/", "."));
+				return instrumentAllMethods(ctClass).toBytecode();
+			} catch (NotFoundException | CannotCompileException | IOException e) {
+				e.printStackTrace();
 			}
-		} catch (NotFoundException e) {
-			e.printStackTrace();
-		} catch (CannotCompileException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
 		return classfileBuffer;
+	}
+
+	private CtClass instrumentAllMethods(CtClass ctClass)
+			throws CannotCompileException {
+		if (!ctClass.isFrozen()) {
+			for (CtMethod declaredMethod : ctClass.getDeclaredMethods()) {
+				declaredMethod.insertBefore(createEnterInstrumentationString(
+						ctClass.getName(), declaredMethod.getName()));
+				declaredMethod.insertAfter(createExitInstrumentationString(
+						ctClass.getName(), declaredMethod.getName()));
+			}
+		}
+		return ctClass;
 	}
 
 	private String createEnterInstrumentationString(String className,
