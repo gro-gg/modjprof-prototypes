4ba7d28f85ed9bf1ba1cd31e7c8705daee447a13
Philipp Grogg <grogg@puzzle.ch>
2015-02-20 17:18:19 +0100

    log method entry added

---
+++ prototype-asm/src/ch/puzzle/profiler/InstrumentMeasurementPonitsMethodVisitor.java
@@ -0,0 +1,49 @@
+package ch.puzzle.profiler;
+
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+public class InstrumentMeasurementPonitsMethodVisitor extends MethodVisitor {
+
+	private String methodName;
+	private String className;
+	
+	public InstrumentMeasurementPonitsMethodVisitor(
+			MethodVisitor methodVisitor, String methodName, String className) {
+		super(Opcodes.ASM5, methodVisitor);
+		this.methodName = methodName;
+		this.className = className;
+	}
+
+	@Override
+	public void visitCode() {
+		mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+		mv.visitLdcInsn(createEnterInstrumentationString(className, methodName));
+		mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
+		super.visitCode();
+	}
+	
+	private String createEnterInstrumentationString(String className,
+			String methodName) {
+		return createInstrumentationString(className, methodName, ">");
+	}
+
+	private String createExitInstrumentationString(String className,
+			String methodName) {
+		return createInstrumentationString(className, methodName, "<");
+	}
+
+	private String createInstrumentationString(String className,
+			String methodName, String flowPattern) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(" ");
+		sb.append(flowPattern);
+		sb.append(" ");
+		sb.append(className);
+		sb.append(".");
+		sb.append(methodName);
+		sb.append("()");
+		return sb.toString();
+	}
+
+}
--- prototype-asm/src/ch/puzzle/profiler/InstrumentMethodClassVisitor.java
+++ prototype-asm/src/ch/puzzle/profiler/InstrumentMethodClassVisitor.java
@@ -8,41 +8,26 @@ public class InstrumentMethodClassVisitor extends ClassVisitor {
 
 	private String className;
 
-	public InstrumentMethodClassVisitor(ClassVisitor classVisitor, String className) {
+	public InstrumentMethodClassVisitor(ClassVisitor classVisitor,
+			String className) {
 		super(Opcodes.ASM5, classVisitor);
 		this.className = className;
 	}
 
 	@Override
-	public MethodVisitor visitMethod(int access, String name, String desc,
-			String signature, String[] exceptions) {
-		System.out.println("   instrumenting method " + name);
-		// + " access:" + access + " desc:" + desc + " signature:" + signature);
-		MethodVisitor methodVisitor = super.visitMethod(access, name, desc, signature, exceptions);
-		return methodVisitor;
-	}
-
-	private String createEnterInstrumentationString(String className,
-			String methodName) {
-		return createInstrumentationString(className, methodName, ">");
-	}
-
-	private String createExitInstrumentationString(String className,
-			String methodName) {
-		return createInstrumentationString(className, methodName, "<");
-	}
-
-	private String createInstrumentationString(String className,
-			String methodName, String flowPattern) {
-		StringBuilder sb = new StringBuilder();
-		sb.append("System.out.println(\" ");
-		sb.append(flowPattern);
-		sb.append(" ");
-		sb.append(className);
-		sb.append(".");
-		sb.append(methodName);
-		sb.append("()\");");
-		return sb.toString();
+	public MethodVisitor visitMethod(int access, String methodName,
+			String desc, String signature, String[] exceptions) {
+		MethodVisitor parentMethodVisitor = super.visitMethod(access, methodName,
+				desc, signature, exceptions);
+		if (!methodName.equals("<init>")) {
+			System.out.println("   instrumenting method " + methodName);
+			// + " access:" + access + " desc:" + desc + " signature:" +
+			// signature);
+			MethodVisitor instrumentMeasurementPonitsMethodVisitor = new InstrumentMeasurementPonitsMethodVisitor(
+					parentMethodVisitor, methodName, className);
+			return instrumentMeasurementPonitsMethodVisitor;
+		}
+		return parentMethodVisitor;
 	}
 
 }
